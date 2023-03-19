92f364108ef15cfe9f0803d0c1e468b4b6b3e080
Renyu Zhang <renyu.zhang@shopee.com>
2023-03-19 23:47:08 +0800

    feat: init project

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,41 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import { useIntl } from 'umi';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: '蚂蚁集团体验技术部出品',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'Sharp Chat',
+          title: 'Sharp Chat',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'Sharp Chat',
+          title: 'Sharp Chat',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
